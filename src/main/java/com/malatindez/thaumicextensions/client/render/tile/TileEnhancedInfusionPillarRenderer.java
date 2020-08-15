fd90faed3796b6fbca6cbf8fdf04094179feb31d
malatindez <axenance@gmail.com>
2020-08-15 10:41:24 +0300

    Enhanced infusion pillar render update

--- src/main/java/com/malatindez/thaumicextensions/client/render/tile/TileEnhancedInfusionPillarRenderer.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/tile/TileEnhancedInfusionPillarRenderer.java
@@ -2,33 +2,64 @@ package com.malatindez.thaumicextensions.client.render.tile;
 
 import com.malatindez.thaumicextensions.ThaumicExtensions;
 import com.malatindez.thaumicextensions.client.lib.UtilsFX;
+import com.malatindez.thaumicextensions.client.render.misc.AdvancedModelRenderer;
+import com.malatindez.thaumicextensions.client.render.misc.Animation;
 import com.malatindez.thaumicextensions.common.tiles.TileEnhancedInfusionPillar;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 import org.lwjgl.opengl.GL11;
-import java.util.concurrent.ThreadLocalRandom;
 
-import static net.minecraft.util.MathHelper.cos;
-import static net.minecraft.util.MathHelper.sin;
+import java.util.ArrayList;
 
 public class TileEnhancedInfusionPillarRenderer extends TileEntitySpecialRenderer {
 
-    private static final ResourceLocation PILLAR1_P1 = new ResourceLocation(ThaumicExtensions.MODID, "models/pillars/pillar1_1.obj");
-    private static final ResourceLocation PILLAR1_P2 = new ResourceLocation(ThaumicExtensions.MODID, "models/pillars/pillar1_2.obj");
-    private IModelCustom PILLAR1_PART1 = AdvancedModelLoader.loadModel(PILLAR1_P1);
-    private IModelCustom PILLAR1_PART2 = AdvancedModelLoader.loadModel(PILLAR1_P2);
+    private static final AdvancedModelRenderer[] models = new AdvancedModelRenderer[] {
+            new AdvancedModelRenderer(
+                    new ResourceLocation(ThaumicExtensions.MODID, "Pillar1.obj"),
+                    new AdvancedModelRenderer.BoundTexture[] {
+                            new AdvancedModelRenderer.BoundTexture(
+                                    ThaumicExtensions.MODID, "textures/model/pillar1_1.png", "base"),
+                            new AdvancedModelRenderer.BoundTexture(
+                                    ThaumicExtensions.MODID, "textures/model/pillar1_2.png", "floating_part"),
+                            new AdvancedModelRenderer.BoundTexture(
+                                    ThaumicExtensions.MODID, "textures/model/pillar1_3.png", "crystal"),
+                            new AdvancedModelRenderer.BoundTexture(
+                                    ThaumicExtensions.MODID, "textures/model/pillar1_4.png", "floating_crystals1"),
+                    },
+                    new Animation[] {
+                            new Animation(null),
+                            new Animation(
+                                    new Animation.SimpleAnimation[] {
+                                            Animation.Wave(0.1f, 10.0f, Animation.Axis.y)
+                                    }
+                            ),
+                            new Animation(
+                                    new Animation.SimpleAnimation[] {
+                                            Animation.Wave(0.1f, 10.0f, Animation.Axis.y),
+                                            Animation.RotateAroundItself(6.0f, Animation.Axis.y)
+                                    }
+                            ),
+                            new Animation(
+                                    new Animation.SimpleAnimation[] {
+                                            Animation.Wave(0.25f, 30.0f, Animation.Axis.y),
+                                            Animation.RotateAroundCenterAtRadius(0,6,0,0.5f)
+                                    }
+                            )
+                    }
+            ),
+    };
+
+
 
     private static final ResourceLocation BALL = new ResourceLocation(ThaumicExtensions.MODID, "models/pillars/ball.obj");
-    private IModelCustom ball2 = AdvancedModelLoader.loadModel(BALL);
+    private IModelCustom ball = AdvancedModelLoader.loadModel(BALL);
 
     public void renderModel(IModelCustom model, int orientation, String texture, float x, float y, float z) {
         GL11.glPushMatrix();
         GL11.glTranslatef(x, y, z);
-        GL11.glRotatef(90.0F, -1.0F, 0.0F, 0.0F);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         if (orientation == 3) {
             GL11.glRotatef(90.0F, 0.0F, 0.0F, 1.0F);
@@ -47,63 +78,39 @@ public class TileEnhancedInfusionPillarRenderer extends TileEntitySpecialRendere
         GL11.glTranslatef(x, y, z);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         UtilsFX.bindTexture(ThaumicExtensions.MODID, "models/pillars/ball.png");
-        this.ball2.renderAll();
+        this.ball.renderAll();
         GL11.glPopMatrix();
     }
-    public void renderFirstTier(TileEnhancedInfusionPillar tile, float x, float y, float z, float o) {
+    public void renderFirstTier(TileEnhancedInfusionPillar tile, float x, float y, float z, double o) {
         this.renderModel(this.PILLAR1_PART1, tile.orientation,
                 "models/pillars/pillar1_1.png",
                 x + 0.5f,
                 y,
                  z + 0.5f);
 
+        float xOffset = 0.75f,  zOffset = 0.75f;
+        if (tile.orientation == 3) {
+            zOffset = 0.25f;
+        } else if (tile.orientation == 4) {
+            xOffset = 0.25f;
+        } else if (tile.orientation == 5) {
+            xOffset = 0.25f;
+            zOffset = 0.25f;
+        }
         this.renderModel(this.PILLAR1_PART2, tile.orientation,
                 "models/pillars/pillar1_2.png",
-                x + 0.5f,
-                y + 2.2f + 0.1f * cos((o+0.1f)/15.0f), // 15s rendering delay
-                z + 0.5f);
-
-        float xOffset = 0.625f,  zOffset = 0.625f;
-        if (tile.orientation == 3) {
-            zOffset = 0.375f;
-        } else if (tile.orientation == 4) {
-            xOffset = 0.375f;
-        } else if (tile.orientation == 5) {
-            xOffset = 0.375f;
-            zOffset = 0.375f;
-        }
+                x + xOffset,
+                y + 2.0f + 0.05f * (float)java.lang.Math.cos((o+0.1)/15.0), // 15s lambda
+                z + zOffset);
         renderBall(x + xOffset,
-                   y + 2.7f + 0.15f * sin((o+0.2f)/30.0f), // 30s rendering delay
+                   y + 2.65f + 0.1f * (float)java.lang.Math.sin((o+0.2)/30.0), // 30s lambda
                    z + zOffset);
     }
+    private long lastTime = 0;
     public void renderTileEntityAt(TileEnhancedInfusionPillar tile, double x, double y, double z, float tick) {
-        float o = (tick + tile.offset) * (float)Math.PI/10.0f;
-        // tick mod 20 -> [0, 2pi]
-        switch(tile.tier) {
-            case 0:
-                renderFirstTier(tile,(float)x,(float)y,(float)z,o);
-        }
-
-
-
-        /*float x = (float)((System.currentTimeMillis()+ tile.offset) % 30000 ) / 30000 * (float)Math.PI;
-        x = 3*cos(sin(cos(x))) - 5.0f/2.0f; // [-0.5, 0.5]
-        x = x * 0.1f; // [-0.5, 0.5] transforming to [-0.1, 0.1]
-
-        this.model2.renderAll();
-        GL11.glPopMatrix();
-        GL11.glPushMatrix();
-        float y = (float)((System.currentTimeMillis()+ tile.offset) % 45000 ) / 45000 * (float)Math.PI;
-        y = 3*cos(sin(cos(y))) - 5.0f/2.0f; // [-0.5, 0.5]
-        y = y * 0.15f; // [-0.5, 0.5] transforming to [-0.2, 0.2]
-
-
-
-        GL11.glTranslatef((float)par2 + xOffset, (float)par4 + 2.7F + y, (float)par6 + zOffset);
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        UtilsFX.bindTexture("thaumicextensions", "models/pillars/ball.png");
-        this.ball2.renderAll();
-        GL11.glPopMatrix();*/
+        double time2pi = (double)(System.currentTimeMillis()+tile.offset) / 1000 * Math.PI * 2;
+        // lambda = 2pi = 1 second
+        models[tile.tier].RenderAll((float)x,(float)y,(float)z, time2pi);
     }
 
     public void renderTileEntityAt(TileEntity par1TileEntity, double par2, double par4, double par6, float par8) {
