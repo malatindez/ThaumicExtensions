c7b2660bfa36943fb07355e716d0425873786579
malatindez <axenance@gmail.com>
2020-08-15 16:55:11 +0300

    Animation rendering fixes and redundant initializer remove

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/Animation.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/Animation.java
@@ -3,10 +3,12 @@ package com.malatindez.thaumicextensions.client.render.misc;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import net.minecraft.client.Minecraft;
 import org.lwjgl.opengl.GL11;
 import sun.java2d.pipe.SpanShapeRenderer;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
@@ -37,20 +39,21 @@ public class Animation {
         float speedZ;
         Type type;
         Axis axis;
-        float noise = 0;
+        float noise;
         protected SimpleAnimation(float speedX, float speedY, float speedZ, float radius) {
             this.type = Type.RotationAroundCenter;
             this.speed = speedX;
             this.speedY = speedY;
             this.speedZ = speedZ;
             this.offset = radius;
+            this.noise = ThreadLocalRandom.current().nextFloat() * ThreadLocalRandom.current().nextInt(10000000);
         }
         protected SimpleAnimation(float speed, float offset, Type type, Axis axis) {
             this.speed = speed;
             this.offset = offset;
             this.type = type;
             this.axis = axis;
-            this.noise = (float)this.hashCode() / 1000000.0f;
+            this.noise = ThreadLocalRandom.current().nextFloat() * ThreadLocalRandom.current().nextInt(10000000);
         }
     }
 
@@ -104,6 +107,8 @@ public class Animation {
     
 
     void PushMatrix(float x, float y, float z, double time2pi) {
+        x += 0.5f; //
+        z += 0.5f; // Move model to the center of a block
         GL11.glPushMatrix();
         for(SimpleAnimation wave : waves) {
             float a = (float)sin((time2pi + wave.noise) / wave.speed) * wave.offset;
@@ -111,30 +116,29 @@ public class Animation {
             else if(wave.axis == Axis.y) { y += a; }
             else if(wave.axis == Axis.z) { z += a; }
         }
-        for(SimpleAnimation rotationAroundItself : rotationsAroundItself) {
-            float a = (float)sin((time2pi + rotationAroundItself.noise) / rotationAroundItself.speed);
-            if (rotationAroundItself.axis == Axis.x)      { GL11.glRotatef(a * 180.0f ,1,0,0); }
-            else if (rotationAroundItself.axis == Axis.y) { GL11.glRotatef(a * 180.0f ,1,0,0); }
-            else if (rotationAroundItself.axis == Axis.z) { GL11.glRotatef(a * 180.0f ,1,0,0); }
-        }
+
+        double m = (time2pi) / Math.PI / 2;
         if(rotationAroundCenter != null) {
+            double n =  m + rotationAroundCenter.noise;
+            float a = (float) ((n * rotationAroundCenter.speed)  % 360.0);
+            float b = (float) ((n * rotationAroundCenter.speedY)  % 360.0);
+            float c = (float) ((n * rotationAroundCenter.speedZ)  % 360.0);
+            GL11.glTranslatef(x,y,z);
+            GL11.glRotatef(a, 1, 0, 0);
+            GL11.glRotatef(b, 0, 1, 0);
+            GL11.glRotatef(c, 0, 0, 1);
             GL11.glTranslatef(
-                    rotationAroundCenter.speed  == 0 ? x : (rotationAroundCenter.offset + x),
-                    rotationAroundCenter.speedY == 0 ? y : (rotationAroundCenter.offset + y),
-                    rotationAroundCenter.speedZ == 0 ? z : (rotationAroundCenter.offset + z));
-            float sx = (float)sin((time2pi + rotationAroundCenter.noise)        / rotationAroundCenter.speed  / 360);
-            float sy = (float)cos((time2pi + rotationAroundCenter.noise)        / rotationAroundCenter.speedY / 360);
-            float sz = (float)sin((time2pi + 1.1f * rotationAroundCenter.noise) / rotationAroundCenter.speedZ / 360);
-            GL11.glRotatef(sx * 180.0f ,1,0,0);
-            GL11.glRotatef(sy * 180.0f ,0,1,0);
-            GL11.glRotatef(sz * 180.0f ,0,0,1);
-            GL11.glTranslatef(
-                    rotationAroundCenter.speed  == 0 ? -x : (-rotationAroundCenter.offset - x),
-                    rotationAroundCenter.speedY == 0 ? -y : (-rotationAroundCenter.offset - y),
-                    rotationAroundCenter.speedZ == 0 ? -z : (-rotationAroundCenter.offset - z));
+                    rotationAroundCenter.speedZ == 0 ? 0 : (rotationAroundCenter.offset),
+                    rotationAroundCenter.speed  == 0 ? 0 : (rotationAroundCenter.offset),
+                    rotationAroundCenter.speedY == 0 ? 0 : (rotationAroundCenter.offset));
         } else {
             GL11.glTranslatef(x, y, z);
         }
-        GL11.glPopMatrix();
+        for(SimpleAnimation rotationAroundItself : rotationsAroundItself) {
+            float a = (float)((m + rotationAroundItself.noise) * rotationAroundItself.speed % 360.0);
+            if (rotationAroundItself.axis == Axis.x)      { GL11.glRotatef(a ,1,0,0); }
+            else if (rotationAroundItself.axis == Axis.y) { GL11.glRotatef(a ,0,1,0); }
+            else if (rotationAroundItself.axis == Axis.z) { GL11.glRotatef(a ,0,0,1); }
+        }
     }
 }
