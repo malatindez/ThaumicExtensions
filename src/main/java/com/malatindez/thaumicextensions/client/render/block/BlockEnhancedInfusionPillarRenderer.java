71142517309788122126887f7c8b0c00db5efaf3
malatindez <axenance@gmail.com>
2020-08-15 23:34:10 +0300

    Client side fix

--- src/main/java/com/malatindez/thaumicextensions/client/render/block/BlockEnhancedInfusionPillarRenderer.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/block/BlockEnhancedInfusionPillarRenderer.java
@@ -3,6 +3,8 @@ package com.malatindez.thaumicextensions.client.render.block;
 import com.malatindez.thaumicextensions.common.ConfigBlocks;
 
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
@@ -10,6 +12,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import org.lwjgl.opengl.GL11;
 
+@SideOnly(Side.CLIENT)
 public class BlockEnhancedInfusionPillarRenderer extends BlockRenderer implements ISimpleBlockRenderingHandler {
     public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
 
