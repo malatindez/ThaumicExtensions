f40670391e259dee1798c525ec0fea7846d9aea8
malatindez <axenance@gmail.com>
2020-08-25 19:07:37 +0300

    dependencies update

--- src/main/java/com/malatindez/thaumicextensions/ThaumicExtensions.java
+++ src/main/java/com/malatindez/thaumicextensions/ThaumicExtensions.java
@@ -1,8 +1,10 @@
 package com.malatindez.thaumicextensions;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod;
@@ -11,7 +13,11 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import com.malatindez.thaumicextensions.client.ClientProxy;
-@Mod(modid = ThaumicExtensions.MODID, version = ThaumicExtensions.VERSION, dependencies = "after:Thaumcraft;after:AWWayofTime;after:Botania")
+import thaumcraft.common.lib.network.EventHandlerNetwork;
+
+@Mod(modid = ThaumicExtensions.MODID, version = ThaumicExtensions.VERSION,
+        guiFactory = "thaumicextensions.client.ThaumicExtensionsGuiFactory",
+        dependencies = "required-after:Forge@[10.13.2,);after:Thaumcraft;after:AWWayofTime;after:Botania")
 public class ThaumicExtensions
 {
     public static final String MODID = "thaumicextensions";
