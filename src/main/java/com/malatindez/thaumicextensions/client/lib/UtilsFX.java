872ca0ad411b387376e18d69cb2d571b6c986f20
malatindez <axenance@gmail.com>
2020-08-25 19:06:38 +0300

    UtilsFX update

--- src/main/java/com/malatindez/thaumicextensions/client/lib/UtilsFX.java
+++ src/main/java/com/malatindez/thaumicextensions/client/lib/UtilsFX.java
@@ -5,12 +5,11 @@ import net.minecraft.util.ResourceLocation;
 
 import java.util.HashMap;
 import java.util.Map;
-
-public class UtilsFX {
+public class UtilsFX extends thaumcraft.client.lib.UtilsFX {
 
     static Map<String, ResourceLocation> boundTextures = new HashMap<String, ResourceLocation>();
     public static void bindTexture(String mod, String texture) {
-        ResourceLocation rl = null;
+        ResourceLocation rl;
         if (boundTextures.containsKey(texture)) {
             rl = boundTextures.get(texture);
         } else {
