fb7413a4bacf3e715d903412f4e6abe8e844404c
malatindez <axenance@gmail.com>
2020-08-25 19:11:16 +0300

    proxy update

--- src/main/java/com/malatindez/thaumicextensions/IProxy.java
+++ src/main/java/com/malatindez/thaumicextensions/IProxy.java
@@ -4,8 +4,11 @@ import com.malatindez.thaumicextensions.common.Config;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 
-public class IProxy {
+public class IProxy implements IGuiHandler {
     public void preInit(FMLPreInitializationEvent e) {
 
     }
@@ -16,5 +19,20 @@ public class IProxy {
 
     public void postInit(FMLPostInitializationEvent e) {
 
+    }
+    public World getClientWorld() {
+        return null;
+    }
+
+
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        return null;
+    }
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+        switch (ID) {
+        }
+        return null;
     }
 }
