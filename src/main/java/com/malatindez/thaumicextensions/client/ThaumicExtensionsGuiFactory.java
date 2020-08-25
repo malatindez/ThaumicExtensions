05a47e8ddc8ae6a40214311b803db794518fe022
malatindez <axenance@gmail.com>
2020-08-25 19:07:06 +0300

    Gui Factory init commit

---
+++ src/main/java/com/malatindez/thaumicextensions/client/ThaumicExtensionsGuiFactory.java
@@ -0,0 +1,24 @@
+package com.malatindez.thaumicextensions.client;
+
+
+import cpw.mods.fml.client.IModGuiFactory;
+import java.util.Set;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import thaumcraft.client.gui.ThaumcraftGuiConfig;
+
+public class ThaumicExtensionsGuiFactory implements IModGuiFactory {
+    public void initialize(Minecraft minecraftInstance) {}
+
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return ThaumcraftGuiConfig.class;
+    }
+
+    public Set<IModGuiFactory.RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
+
+    public IModGuiFactory.RuntimeOptionGuiHandler getHandlerFor(IModGuiFactory.RuntimeOptionCategoryElement element) {
+        return null;
+    }
+}
