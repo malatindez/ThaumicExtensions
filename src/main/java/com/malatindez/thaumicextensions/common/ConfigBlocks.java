4871eb18da97209305b1050bfc7f3874cce99135
malatindez <axenance@gmail.com>
2020-08-25 19:12:58 +0300

    config blocks update

--- src/main/java/com/malatindez/thaumicextensions/common/ConfigBlocks.java
+++ src/main/java/com/malatindez/thaumicextensions/common/ConfigBlocks.java
@@ -1,7 +1,6 @@
 package com.malatindez.thaumicextensions.common;
 
 import com.malatindez.thaumicextensions.common.blocks.BlockEnhancedInfusionPillar;
-import com.malatindez.thaumicextensions.common.blocks.BlockInfusionStabiliser;
 import com.malatindez.thaumicextensions.common.tiles.TileEnhancedInfusionPillar;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.material.Material;
@@ -16,7 +15,6 @@ public class ConfigBlocks {
 
 
     private static void registerBlocks() {
-        GameRegistry.registerBlock( new BlockInfusionStabiliser(Material.iron, "infusionStabiliser",null), "infusionStabiliser");
         GameRegistry.registerBlock( new BlockEnhancedInfusionPillar(), "blockInfusionPillar");
     }
 
