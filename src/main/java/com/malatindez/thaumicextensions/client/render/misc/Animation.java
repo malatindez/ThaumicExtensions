f459d23f5a025cc5b014abafaadba9a062d5b30c
malatindez <axenance@gmail.com>
2020-08-26 19:51:44 +0300

    Transformation refactor

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/Animation.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/Animation.java
@@ -107,23 +107,22 @@ public class Animation {
     
 
 
-    Transformation getModifiedCoordinates(Transformation transform, double noise) {
+    Vector3f getModifiedCoordinates(Transformation transform, double noise) {
         Matrix4f matrix = new Matrix4f();
         matrix.m00 = matrix.m11 = matrix.m22 = matrix.m33 = 1;
         double time = ((double)Minecraft.getSystemTime()) / 1000;
         for(SimpleAnimation wave : waves) {
             float a = (float)sin((time*2*Math.PI + wave.noise + noise) / wave.speed) * wave.amplitude;
-            if(wave.axis == Axis.x) { transform.x += a; }
-            else if(wave.axis == Axis.y) { transform.y += a; }
-            else if(wave.axis == Axis.z) { transform.z += a; }
+            if(wave.axis == Axis.x) { transform.position.x += a; }
+            else if(wave.axis == Axis.y) { transform.position.y += a; }
+            else if(wave.axis == Axis.z) { transform.position.z += a; }
         }
         if(rotationAroundCenter != null) {
             double n =  time + rotationAroundCenter.noise  + noise;
             float a = (float) ((n * rotationAroundCenter.speed)  % 360.0);
             float b = (float) ((n * rotationAroundCenter.speedY)  % 360.0);
             float c = (float) ((n * rotationAroundCenter.speedZ)  % 360.0);
-            Vector3f vec = new Vector3f(transform.x,transform.y,transform.z);
-            matrix.translate(vec);
+            matrix.translate(transform.position);
             matrix.rotate(a / 180.0f * (float)Math.PI, new Vector3f(1,0,0));
             matrix.rotate(b / 180.0f * (float)Math.PI, new Vector3f(0,1,0));
             matrix.rotate(c / 180.0f * (float)Math.PI, new Vector3f(0,0,1));
@@ -138,25 +137,30 @@ public class Animation {
                     rotationAroundCenter.speedY == 0 ? 0 : (radius)));
         }
         else {
-            matrix.translate(new Vector3f(transform.x, transform.y, transform.z));
+            matrix.translate(new Vector3f(transform.position.x, transform.position.y, transform.position.z));
         }
-        return new Transformation(matrix.m30,matrix.m31,matrix.m32);
+        return new Vector3f(matrix.m30,matrix.m31,matrix.m32);
     }
-    void PushMatrix(Transformation transform, double noise) {
+    /**
+     * Pushes matrix onto stack according to the current time & noise
+     * @param transform - current object position, rotation, scale and color
+     * @param noise - TileEntity noise, which will be used as unique offset for this object
+     */
+    void getMatrix(Transformation transform, double noise) {
         double time = ((double)Minecraft.getSystemTime()) / 1000;
         GL11.glPushMatrix();
         for(SimpleAnimation wave : waves) {
             float a = (float)sin((time*2*Math.PI + wave.noise + noise) / wave.speed) * wave.amplitude;
-            if(wave.axis == Axis.x) { transform.x += a; }
-            else if(wave.axis == Axis.y) { transform.y += a; }
-            else if(wave.axis == Axis.z) { transform.z += a; }
+            if(wave.axis == Axis.x) { transform.position.x += a; }
+            else if(wave.axis == Axis.y) { transform.position.y += a; }
+            else if(wave.axis == Axis.z) { transform.position.z += a; }
         }
         if(rotationAroundCenter != null) {
             double n =  time + rotationAroundCenter.noise  + noise;
             float a = (float) ((n * rotationAroundCenter.speed)  % 360.0);
             float b = (float) ((n * rotationAroundCenter.speedY)  % 360.0);
             float c = (float) ((n * rotationAroundCenter.speedZ)  % 360.0);
-            GL11.glTranslatef(transform.x,transform.y,transform.z);
+            GL11.glTranslatef(transform.position.x, transform.position.y, transform.position.z);
             GL11.glRotatef(a, 1, 0, 0);
             GL11.glRotatef(b, 0, 1, 0);
             GL11.glRotatef(c, 0, 0, 1);
@@ -170,7 +174,7 @@ public class Animation {
                     rotationAroundCenter.speed  == 0 ? 0 : (radius),
                     rotationAroundCenter.speedY == 0 ? 0 : (radius));
         } else {
-            GL11.glTranslatef(transform.x, transform.y, transform.z);
+            GL11.glTranslatef(transform.position.x, transform.position.y, transform.position.z);
         }
         for(SimpleAnimation rotationAroundItself : rotationsAroundItself) {
             float a = (float)((time + rotationAroundItself.noise  + noise) * rotationAroundItself.speed % 360.0);
@@ -178,14 +182,11 @@ public class Animation {
             else if (rotationAroundItself.axis == Axis.y) { GL11.glRotatef(a ,0,1,0); }
             else if (rotationAroundItself.axis == Axis.z) { GL11.glRotatef(a ,0,0,1); }
         }
-        GL11.glRotatef(transform.degreeX,1,0,0);
-        GL11.glRotatef(transform.degreeY,0,1,0);
-        GL11.glRotatef(transform.degreeZ,0,0,1);
-        GL11.glScalef(
-                transform.scaleX,
-                transform.scaleY,
-                transform.scaleZ);
-        GL11.glColor4f(transform.colorR,transform.colorG,transform.colorB,transform.colorA);
+        GL11.glRotatef(transform.degree.x,1,0,0);
+        GL11.glRotatef(transform.degree.y,0,1,0);
+        GL11.glRotatef(transform.degree.z,0,0,1);
+        GL11.glScalef(transform.scale.x, transform.scale.y, transform.scale.z);
+        GL11.glColor4f(transform.color.x, transform.color.y, transform.color.z, transform.color.w);
     }
 
 }
