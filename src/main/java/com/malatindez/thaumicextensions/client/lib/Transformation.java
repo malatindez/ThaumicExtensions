96e97074ad94f673768375ff1164ef1a423331c3
malatindez <axenance@gmail.com>
2020-08-26 19:17:57 +0300

    Transformation refactor

--- src/main/java/com/malatindez/thaumicextensions/client/lib/Transformation.java
+++ src/main/java/com/malatindez/thaumicextensions/client/lib/Transformation.java
@@ -1,39 +1,58 @@
 package com.malatindez.thaumicextensions.client.lib;
 
 import com.malatindez.thaumicextensions.client.render.misc.Animation;
+import com.sun.istack.internal.NotNull;
+import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.util.vector.Vector4f;
 
 public class Transformation {
-    public float x, y, z;
-    public float degreeX = 0, degreeY = 0, degreeZ = 0;
-    public float scaleX = 1, scaleY = 1, scaleZ = 1;
-    public float colorR = 1, colorG = 1, colorB = 1, colorA = 1;
+    public final Vector3f position = new Vector3f(0,0,0);
+    public final Vector3f degree = new Vector3f(0,0,0);
+    public final Vector3f scale = new Vector3f(1,1,1);
+    public final Vector4f color = new Vector4f(1,1,1,1);
     public Transformation() {}
     public Transformation(float x, float y, float z) {
-        this.x = x; this.y = y; this.z = z;
+        this.position.set(x,y,z);
     }
     public Transformation(float x, float y, float z,
                           float degreeX, float degreeY, float degreeZ) {
-        this(x,y,z);
-        this.degreeX = degreeX; this.degreeY = degreeY; this.degreeZ = degreeZ;
+        this(x, y, z);
+        this.degree.set(degreeX, degreeY, degreeZ);
     }
-    public Transformation(float x, float y, float z,
+    public Transformation(float x,       float y,       float z,
                           float degreeX, float degreeY, float degreeZ,
-                          float scaleX, float scaleY, float scaleZ) {
-        this(x,y,z,degreeX,degreeY,degreeZ);
-        this.scaleX = scaleX; this.scaleY = scaleY; this.scaleZ = scaleZ;
+                          float scaleX,  float scaleY,  float scaleZ) {
+        this(   x,       y,       z,
+                degreeX, degreeY, degreeZ);
+        this.scale.set(scaleX, scaleY, scaleZ);
     }
-    public Transformation(float x, float y, float z,
+    public Transformation(float x,       float y,       float z,
                           float degreeX, float degreeY, float degreeZ,
-                          float scaleX, float scaleY, float scaleZ,
-                          float colorR, float colorG, float colorB, float colorA) {
-        this(x,y,z,degreeX,degreeY,degreeZ,scaleX,scaleY,scaleZ);
-        this.colorR = colorR; this.colorG = colorG; this.colorB = colorB; this.colorA = colorA;
+                          float scaleX,  float scaleY,  float scaleZ,
+                          float colorR,  float colorG,  float colorB, float colorA) {
+        this(   x,       y,       z,
+                degreeX, degreeY, degreeZ,
+                scaleX,  scaleY,  scaleZ);
+        this.color.set(colorR, colorG, colorB, colorA);
     }
-
-    public Transformation(Transformation transform) {
-        x =  transform.x; y = transform.y; z = transform.z;
-        degreeX = transform.degreeX; degreeY = transform.degreeY; degreeZ = transform.degreeZ;
-        scaleX = transform.scaleX; scaleY = transform.scaleY; scaleZ = transform.scaleZ;
-        colorR = transform.colorR; colorG = transform.colorG; colorB = transform.colorB; colorA = transform.colorA;
+    public Transformation(@NotNull final Vector3f position) {
+        this.position.set(position);
+    }
+    public Transformation(@NotNull final Vector3f position, @NotNull final Vector3f degree) {
+        this(position);
+        this.degree.set(degree);
+    }
+    public Transformation(@NotNull final Vector3f position, @NotNull final Vector3f degree,
+                          @NotNull final Vector3f scale) {
+        this(position, degree);
+        this.scale.set(scale);
+    }
+    public Transformation(@NotNull final Vector3f position, @NotNull final Vector3f degree,
+                          @NotNull final Vector3f scale,    @NotNull final Vector4f color) {
+        this(position, degree, scale);
+        this.color.set(color);
+    }
+    public Transformation(@NotNull final Transformation transform) {
+        this(transform.position, transform.degree, transform.scale, transform.color);
     }
 }
