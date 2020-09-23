39430528d8e7f956dc656a29c1c66c2f8261de2f
malatindez <axenance@gmail.com>
2020-09-23 21:32:26 +0300

    Drag update

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/Drag.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/Drag.java
@@ -6,24 +6,15 @@ import org.lwjgl.util.vector.Vector4f;
 
 import java.lang.reflect.Method;
 
-public class Drag extends DefaultGuiObject implements EnhancedGuiScreen.Clickable, EnhancedGuiScreen.needParent {
-    protected DefaultGuiObject icon;
+public class Drag extends Collection {
     protected final Object obj;
-    protected final Method currentlyDragged;
+    protected final Method currentlyDragging;
     protected final Method dragEnd;
-    protected Collection parent;
 
-    protected Vector4f dragBorders;
     protected Vector2f previousMousePos = null;
+    protected DefaultGuiObject objectToFocusOn;
+    private boolean currently_dragging = false;
 
-    protected void updateBorders() {
-        borders.set(
-                coordinates.x,
-                coordinates.y,
-                icon.getSize().x + coordinates.x,
-                icon.getSize().y + coordinates.y
-        );
-    }
     public void setCoordinates(Vector2f newCoordinates) {
         this.coordinates.set(newCoordinates);
         updateBorders();
@@ -31,100 +22,99 @@ public class Drag extends DefaultGuiObject implements EnhancedGuiScreen.Clickabl
     public Vector2f getCoordinates() {
         return new Vector2f(coordinates);
     }
-    public void setDragBorders(Vector4f vector) {
-        this.dragBorders.set(vector);
-    }
     public Vector4f getDragBorders() {
-        return dragBorders;
+        return objectToFocusOn.borders;
     }
     /**
      * Drag constructor
-     * @param icon object texture
      * @param obj object which has currentlyDragged and dragEnd methods.
      *            This parameter can be null.
-     * @param currentlyDragged object method which has to have a Vector2f parameter which means current position.
+     * @param currentlyDragging object method which has to have a Vector2f parameter which means current position.
      *                         This parameter can be null.
      * @param dragEnd object method which has to have a Vector2f parameter which means current position.
      *                This parameter can be null.
      */
-    public Drag(@NotNull GuiTextureMapping.Icon icon, Vector2f coordinates,
-                Vector4f dragBorders, int zLevel, Object obj, Method currentlyDragged, Method dragEnd) {
-        super(coordinates,new Vector2f(1.0f, 1.0f),icon.getSize(), zLevel);
-        this.icon = icon;
-        this.dragBorders = new Vector4f(dragBorders);
+    public Drag(Collection collection, Object obj, Method currentlyDragging, Method dragEnd) {
+        super(collection);
         this.obj = obj;
-        this.currentlyDragged = currentlyDragged;
+        this.currentlyDragging = currentlyDragging;
         this.dragEnd = dragEnd;
     }
-
-    public void drag(@NotNull Vector2f currentMousePos) {
-        if(previousMousePos == null) {
-            previousMousePos = new Vector2f(currentMousePos);
-        } else {
-            coordinates.set(
-                    coordinates.x + previousMousePos.x - currentMousePos.x,
-                    coordinates.y + previousMousePos.y - currentMousePos.y
-            );
-            previousMousePos.set(currentMousePos);
-        }
-        if(obj != null && dragEnd != null) {
-            try {
-                currentlyDragged.invoke(obj, coordinates);
-            } catch (Exception ignored) {}
-        }
-    }
-    public void dragEnd() {
-        previousMousePos = null;
-        if(obj != null && dragEnd != null) {
-            try {
-                dragEnd.invoke(obj, coordinates);
-            } catch (Exception ignored) {}
-        }
-    }
-    @Override
-    public void render() {
-        this.render(null);
+    public void DragFocus(DefaultGuiObject objectToFocusOn) {
+        this.objectToFocusOn = objectToFocusOn;
     }
 
-    @Override
-    public void render(Vector2f coordinates) {
-        icon.render(coordinates);
-    }
-
-    @Override
-    public boolean scalable() {
-        return false;
-    }
-
-    @Override
-    public void render(Vector2f coordinates, Vector2f scale) {
-        icon.render(coordinates);
-    }
-
-
-    @Override
-    public Vector4f getBorders() {
-        return borders;
-    }
 
     @Override
     public boolean mouseHandler(Vector2f currentMousePosition) {
-        return false;
+        if(currently_dragging) {
+            if (previousMousePos == null) {
+                previousMousePos = new Vector2f(currentMousePosition);
+            } else {
+                coordinates.set(
+                        coordinates.x - previousMousePos.x + currentMousePosition.x,
+                        coordinates.y - previousMousePos.y + currentMousePosition.y
+                );
+                previousMousePos.set(currentMousePosition);
+            }
+            if (obj != null && dragEnd != null) {
+                try {
+                    currentlyDragging.invoke(obj, coordinates);
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        return super.mouseHandler(currentMousePosition);
+    }
+    @Override
+    public void Update(int flags) {
+        /**
+         * If LMB not pressed -> dragging ends
+         */
+        if(currently_dragging) {
+            currently_dragging = currently_dragging && ((flags & EnhancedGuiScreen.Updatable.Flags.LMB.getType()) != 0);
+            if(!currently_dragging) {
+                previousMousePos = null;
+                if (obj != null && dragEnd != null) {
+                    try {
+                        dragEnd.invoke(obj, coordinates);
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        }
+        super.Update(flags);
     }
 
+    Vector2f currentObjectCoordinates = new Vector2f();
+    @Override
+    public void render() {
+        currentObjectCoordinates.set(coordinates);
+    }
+    @Override
+    public void render(@NotNull Vector2f coordinates) {
+        Vector2f.add(coordinates,this.coordinates,currentObjectCoordinates);
+        super.render(coordinates);
+    }
+    @Override
+    public void render(@NotNull Vector2f coordinates, @NotNull Vector2f scale) {
+        Vector2f.add(coordinates,this.coordinates,currentObjectCoordinates);
+        super.render(coordinates, scale);
+    }
     @Override
     public boolean mouseClicked(Vector2f currentMousePosition, int button) {
-        return false;
-    }
 
-    @Override
-    public int getZLevel() {
-        return 0;
-    }
-
-    @Override
-    public void setParent(Collection parent) {
-        this.parent = parent;
+        if(objectToFocusOn != null &&
+           objectToFocusOn.borders.x < currentObjectCoordinates.x + currentMousePosition.x &&
+           objectToFocusOn.borders.z > currentObjectCoordinates.x + currentMousePosition.x &&
+           objectToFocusOn.borders.y < currentObjectCoordinates.y + currentMousePosition.y &&
+           objectToFocusOn.borders.w > currentObjectCoordinates.y + currentMousePosition.y &&
+           button == 0
+        ) {
+            currently_dragging = true;
+            return true;
+        }
+        return super.mouseClicked(currentMousePosition, button);
     }
 }
 
