a8371cc44c2a127d283cd0c8316e7f651ac22d77
malatindez <axenance@gmail.com>
2020-09-24 01:51:08 +0300

    Update TextLine.java

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/TextLine.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/TextLine.java
@@ -3,28 +3,26 @@ package com.malatindez.thaumicextensions.client.render.misc.GUI;
 import net.minecraft.client.gui.FontRenderer;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
 
 public class TextLine extends DefaultGuiObject {
     protected final FontRenderer fontRendererObj;
     public final String textLine;
-    public int color;
-    public boolean scalable;
+    public Vector3f color;
     public boolean dropShadow;
     public TextLine(
-            FontRenderer fontRendererObj, String textLine, int color, boolean dropShadow, boolean scalable,
-            Vector2f coordinates, Vector2f scale, int zLevel) {
+            FontRenderer fontRendererObj, String textLine, Vector3f color, boolean dropShadow,
+            Vector2f coordinates, Vector2f scale, int zLevel, ResolutionRescaleType type) {
         super(coordinates, scale,
                 new Vector2f(fontRendererObj.getStringWidth(textLine) * scale.x,
                         fontRendererObj.FONT_HEIGHT * scale.y
-                        ), zLevel
+                        ), zLevel, type
                 );
         this.fontRendererObj = fontRendererObj;
         this.color = color;
         this.dropShadow = dropShadow;
-        this.scalable = scalable;
         this.textLine = textLine;
     }
-
     @Override
     protected void VectorsWereUpdated() {
 
@@ -44,12 +42,8 @@ public class TextLine extends DefaultGuiObject {
         GL11.glPushMatrix();
         GL11.glTranslatef(getCurrentPosition().x, getCurrentPosition().y, 0);
         GL11.glScalef(getScale().x, getScale().y, 1);
-        this.fontRendererObj.drawString(textLine, 0, 0, color, dropShadow);
+        this.fontRendererObj.drawString(textLine, 0, 0, (int) (color.x * 255 * 255 + color.y * 255 + color.z), dropShadow);
         GL11.glPopMatrix();
     }
 
-    @Override
-    public boolean scalable() {
-        return scalable;
-    }
 }
