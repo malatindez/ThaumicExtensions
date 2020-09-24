05a8fccf8eb25470e449cc16eb0bbef5d5570e86
malatindez <axenance@gmail.com>
2020-09-24 03:50:20 +0300

    Update IconFactory.java

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/IconFactory.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/IconFactory.java
@@ -2,12 +2,21 @@ package com.malatindez.thaumicextensions.client.render.misc.GUI;
 
 import com.malatindez.thaumicextensions.ThaumicExtensions;
 import com.malatindez.thaumicextensions.client.lib.UtilsFX;
-import com.sun.istack.internal.NotNull;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
 import org.lwjgl.util.vector.Vector2f;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Set;
 
 
 /**
@@ -28,9 +37,9 @@ public class IconFactory {
          * @param textureSize The size of the texture
          * @param scale The scale of an icon (1.0f, 1.0f is a default scale)
          */
-        public Icon(@NotNull Vector2f coordinates, @NotNull Vector2f texFrom,
-                    @NotNull Vector2f iconSize,    @NotNull Vector2f textureSize,
-                    @NotNull Vector2f scale, int zLevel, ResolutionRescaleType type, ResourceLocation texture) {
+        public Icon(Vector2f coordinates, Vector2f texFrom,
+                    Vector2f iconSize,    Vector2f textureSize,
+                    Vector2f scale, int zLevel, ResolutionRescaleType type, ResourceLocation texture) {
             super(coordinates,scale,iconSize, zLevel, type);
             this.texFrom     = new Vector2f(texFrom);
             this.textureSize = new Vector2f(textureSize);
@@ -38,8 +47,8 @@ public class IconFactory {
             this.texture = new ResourceLocation(texture.getResourceDomain(), texture.getResourcePath());
         }
 
-        public Icon(Icon icon, Vector2f coordinates, Vector2f scale, int zLevel) {
-            super(coordinates, scale, icon.getSize(), zLevel, icon.getType());
+        public Icon(Icon icon, Vector2f coordinates, Vector2f scale, int zLevel, ResolutionRescaleType type) {
+            super(coordinates, scale, icon.getSize(), zLevel, type);
             this.texFrom     = new Vector2f(icon.texFrom);
             this.textureSize = new Vector2f(icon.textureSize);
             this.texTo       = new Vector2f(icon.texTo);
@@ -54,7 +63,7 @@ public class IconFactory {
          * @param dest The destination vector, or null if a new vector is to be created
          * @return the mul of left and right in dest
          */
-        private Vector2f mul(@NotNull Vector2f left, @NotNull Vector2f right, Vector2f dest) {
+        private Vector2f mul(Vector2f left, Vector2f right, Vector2f dest) {
             if(dest == null) {
                 return new Vector2f(left.x * right.x, left.y * right.y);
             } else {
@@ -90,20 +99,73 @@ public class IconFactory {
     }
     protected final HashMap<String, Icon> parts = new HashMap<String, Icon>();
     private final ResourceLocation texture;
+    private Vector2f Json2Vec(Object array) {
+        return new Vector2f(
+                ((Long)((JSONArray)array).get(0)).floatValue(),
+                ((Long)((JSONArray)array).get(1)).floatValue()
+        );
+    }
     /**
      * Constructor of IconFactory
      * @param json_file json_file to work with
      */
     public IconFactory(ResourceLocation json_file) {
-        this.texture = new ResourceLocation(ThaumicExtensions.MODID, "asd");
+        InputStream x;
+        try {
+            x = Minecraft.getMinecraft().getResourceManager().getResource(json_file).getInputStream();
+        } catch (Exception e) {
+            System.out.println("Exception caught! Wrong ResourceFile for IconFactory.");
+            System.out.println(json_file);
+            e.printStackTrace();
+            this.texture = new ResourceLocation(ThaumicExtensions.MODID, "asd");
+            return;
+        }
+        InputStreamReader isReader = new InputStreamReader(x);
+        //Creating a BufferedReader object
+        BufferedReader reader = new BufferedReader(isReader);
+        StringBuffer sb = new StringBuffer();
+        String str = "";
+        try {
+            while ((str = reader.readLine()) != null) {
+                sb.append(str);
+            }
+        } catch (Exception ignored) { }
+        str = sb.toString();
+        JSONObject jsonObject = (JSONObject) JSONValue.parse(str);
+
+        this.texture = new ResourceLocation(
+                (String)jsonObject.get("resource_domain"),
+                (String)jsonObject.get("resource_path"));
+
+        Vector2f textureSize = Json2Vec(jsonObject.get("textureSize"));
+        Set<Object> set = jsonObject.keySet();
+        for (Object object : set) {
+            if (object instanceof String) {
+                if (!((String)object).equals("resource_domain") &&
+                        !((String)object).equals("resource_path") &&
+                        !((String)object).equals("textureSize")
+                ) {
+                    parts.put((String)object, new Icon(
+                            new Vector2f(0,0),
+                            Json2Vec(((JSONObject) jsonObject.get(object)).get("texFrom")),
+                            Json2Vec(((JSONObject) jsonObject.get(object)).get("iconSize")),
+                            textureSize,
+                            new Vector2f(1,1),
+                            0,
+                            DefaultGuiObject.ResolutionRescaleType.NONE,
+                            texture
+                    ));
+                }
+            }
+        }
     }
 
     /**
      * @param name Elements name
      * @return element
      */
-    public Icon getNewIcon(final String name, Vector2f coordinates, Vector2f scale, int zLevel) {
-        return new Icon(parts.get(name), coordinates, scale, zLevel);
+    public Icon getNewIcon(final String name, Vector2f coordinates, Vector2f scale, int zLevel, DefaultGuiObject.ResolutionRescaleType type) {
+        return new Icon(parts.get(name), coordinates, scale, zLevel, type);
     }
 
 }
