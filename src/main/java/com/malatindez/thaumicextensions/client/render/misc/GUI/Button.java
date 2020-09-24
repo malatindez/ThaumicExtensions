60f1dd9b75fc2f4b00fca30e7622b1858831ee56
malatindez <axenance@gmail.com>
2020-09-24 15:24:20 +0300

    Update Button.java

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/Button.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/GUI/Button.java
@@ -1,22 +1,23 @@
 package com.malatindez.thaumicextensions.client.render.misc.GUI;
 
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector4f;
+import scala.util.parsing.json.JSON;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class Button extends DefaultGuiObject
-        implements EnhancedGuiScreen.Clickable, EnhancedGuiScreen.Bindable, EnhancedGuiScreen.needParent {
+        implements EnhancedGuiScreen.Clickable {
     protected final Object icon;
-    protected final Object obj;
-    protected final Method method;
-    protected final ArrayList<EnhancedGuiScreen.Bind> binds;
+    protected final MethodObjectPair method;
     protected Collection parent = null;
     protected int zLevel, id;
 
-    /**
+    /*
      * Button constructor
      * @param icon buttons icon which should be rendered, icon type should be DefaultGuiObject
      * @param obj object where a method is located
@@ -25,10 +26,10 @@ public class Button extends DefaultGuiObject
      *                   * Param Object which will be set with this class
      *                   * Param int id, special number which you can add in constructor to identify caller
      */
-    public Button(String name, Object icon, Vector2f coordinates,
+   /* public Button(String name, Object icon, Vector2f coordinates,
                   Object obj, Method method, int id, int zLevel, ResolutionRescaleType type,
                   ArrayList<EnhancedGuiScreen.Bind> binds) {
-        super(name, coordinates,new Vector2f(1.0f, 1.0f),((DefaultGuiObject)icon).getSize(), zLevel, type, false);
+        super(name, coordinates,new Vector2f(1.0f, 1.0f),((DefaultGuiObject)icon).getSize(), zLevel, type);
         this.icon = icon;
         this.obj = obj;
         this.method = method;
@@ -36,9 +37,24 @@ public class Button extends DefaultGuiObject
         this.zLevel = zLevel;
         this.id = id;
         updateVectors();
+    }*/
+    public Button(String name, Object parent, JSONObject parameters) {
+        super(name, parent, parameters);
+        if(parameters.containsKey("method") && parent instanceof DefaultGuiObject) {
+            method = ((DefaultGuiObject) parent).getMethod(
+                    (String) parameters.get("method"),
+                    new Class[] {Object.class, int.class});
+        } else {
+            method = null;
+        }
+        if(parameters.containsKey("id")) {
+            id = (Integer)parameters.get("id");
+        } else {
+            id = 0;
+        }
+        JSONObject icon = (JSONObject) parameters.get(parameters.get("icon"));
+        this.icon = EnhancedGuiScreen.createObject(name, this, icon);
     }
-
-
 
     @Override
     public void render() {
@@ -63,9 +79,9 @@ public class Button extends DefaultGuiObject
 
     @Override
     public boolean mouseClicked(Vector2f currentMousePosition, int button) {
-        if(button == 0) {
+        if(button == 0 && method != null) {
             try {
-                method.invoke(obj, this, id);
+                method.method.invoke(method.object,this, id);
             } catch (Exception ignored) {
                 return false;
             }
@@ -73,13 +89,5 @@ public class Button extends DefaultGuiObject
         return true;
     }
 
-    @Override
-    public ArrayList<EnhancedGuiScreen.Bind> getBinds() {
-        return binds;
-    }
 
-    @Override
-    public void setParent(Collection parent) {
-        this.parent = parent;
-    }
 }
