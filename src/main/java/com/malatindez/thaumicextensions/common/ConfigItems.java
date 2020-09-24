217d1b0d9372bf95374b231d64c5d7b9c1764479
malatindez <axenance@gmail.com>
2020-09-25 00:58:44 +0300

    Update ConfigItems.java

--- src/main/java/com/malatindez/thaumicextensions/common/ConfigItems.java
+++ src/main/java/com/malatindez/thaumicextensions/common/ConfigItems.java
@@ -1,16 +1,20 @@
 package com.malatindez.thaumicextensions.common;
 
 import com.malatindez.thaumicextensions.common.items.ItemEnhancedThaumonomicon;
+import com.malatindez.thaumicextensions.common.items.ItemGuiEditor;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 
 public class ConfigItems {
     public static Item itemThaumonomicon;
+    public static Item itemGuiEditor;
     public static void init() {
         initializeItems();
     }
     private static void initializeItems() {
         itemThaumonomicon = (new ItemEnhancedThaumonomicon()).setUnlocalizedName("ItemThaumonomicon");
         GameRegistry.registerItem(itemThaumonomicon, "ItemThaumonomicon", "Thaumcraft");
+        itemGuiEditor = (new ItemGuiEditor()).setUnlocalizedName("ItemGuiEditor");
+        GameRegistry.registerItem(itemGuiEditor, "ItemGuiEditor", "Thaumcraft");
     }
 }
