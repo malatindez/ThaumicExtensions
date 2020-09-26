c3891bfadeda15cdd2cf8f43782f51013d9cb53e
malatindez <axenance@gmail.com>
2020-09-26 12:10:55 +0300

    Update TextLine.java

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/TextLine.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/TextLine.java
@@ -9,25 +9,31 @@ import org.lwjgl.util.vector.Vector4f;
 
 public class TextLine extends DefaultGuiObject {
     protected FontRenderer fontRendererObj;
-    public String textLine;
+    protected String textLine;
+    protected int renderCursor = 0;
     public Vector4f color;
     public boolean dropShadow;
-
+    protected boolean cutLine = true;
+    Minecraft mc;
     @Override
     public void preInit(String name, Object parent, JSONObject parameters) {
         fontRendererObj = Minecraft.getMinecraft().fontRenderer;
         textLine = (String)parameters.get("text");
-        color = Json4Vec(parameters.get("color"));
+        if(parameters.containsKey("color")) {
+            color = Json4Vec(parameters.get("color"));
+        } else {
+            color = new Vector4f(1,1,1,1);
+        }
         if(parameters.containsKey("dropShadow")) {
             dropShadow = (Boolean)parameters.get("dropShadow");
         } else {
             dropShadow = false;
         }
+        mc = Minecraft.getMinecraft();
     }
 
     @Override
     public void postInit() {
-
     }
 
     public TextLine(String name, Object parent, JSONObject parameters) {
@@ -51,18 +57,33 @@ public class TextLine extends DefaultGuiObject {
         return null;
     }
 
-    @Override
-    protected void VectorsWereUpdated() {
-
+    public void setTextLine(String line) {
+        this.textLine = line;
+        textLineWasUpdated();
+    }
+    public String getTextLine() {
+        return textLine;
     }
 
+    protected String lineToRender;
+    void textLineWasUpdated() {
+        int maxSize = (int) getSize().x;
+        lineToRender = fontRendererObj.trimStringToWidth(textLine.substring(renderCursor), maxSize);
+        if(lineToRender.length() != 0 && cutLine) {
+            String substring = lineToRender.substring(0,lineToRender.length() - 1);
+            int threeDotsSize = fontRendererObj.getStringWidth("...");
+            while (substring.length() != 0) {
+                if (fontRendererObj.getStringWidth(substring) + threeDotsSize > maxSize) {
+                    lineToRender = substring + "...";
+                    break;
+                }
+                substring = substring.substring(0,substring.length() - 1);
+            }
+        }
+    }
     @Override
-    public void reScale(Vector2f scale) {
-        super.reScale(scale);
-        this.setSize(
-                new Vector2f(fontRendererObj.getStringWidth(textLine) * scale.x,
-                        fontRendererObj.FONT_HEIGHT * scale.y
-                ));
+    protected void VectorsWereUpdated() {
+        textLineWasUpdated();
     }
     @Override
     public void render() {
@@ -72,7 +93,7 @@ public class TextLine extends DefaultGuiObject {
         GL11.glPushMatrix();
         GL11.glTranslatef(getCurrentPosition().x, getCurrentPosition().y, 0);
         GL11.glScalef(getScale().x, getScale().y, 1);
-        this.fontRendererObj.drawString(textLine, 0, 0,
+        this.fontRendererObj.drawString(lineToRender, 0, 0,
                 Vector4fToColor(color),
                 dropShadow);
         GL11.glPopMatrix();
