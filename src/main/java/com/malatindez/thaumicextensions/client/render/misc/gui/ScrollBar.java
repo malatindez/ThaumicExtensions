0f17c047421968fdffecd7f4736cb0c7447114f1
malatindez <axenance@gmail.com>
2020-09-29 03:14:21 +0300

    Create ScrollBar.java

---
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/ScrollBar.java
@@ -0,0 +1,59 @@
+package com.malatindez.thaumicextensions.client.render.misc.gui;
+
+import org.json.simple.JSONObject;
+
+public class ScrollBar extends DefaultGuiObject {
+    public interface Scrollable{
+        void setOffsetX(float offset);
+        float getOffsetX();
+        float getSizeX();
+        void setOffsetY(float offset);
+        float getOffsetY();
+        float getSizeY();
+    }
+    public ScrollBar(String name, Object parent, JSONObject parameters) {
+        super(name, parent, parameters);
+    }
+
+    @Override
+    public MethodObjectPair getMethodDown(String objectName, String name, Class[] parameterTypes) {
+        return null;
+    }
+
+    @Override
+    public Object getObjectDown(String objectName) {
+        return null;
+    }
+
+    @Override
+    protected void VectorsWereUpdated() {
+
+    }
+
+    @Override
+    public void preInit(String name, Object parent, JSONObject parameters) {
+
+    }
+    protected DefaultGuiObject objectToScroll;
+    @Override
+    public void postInit() {
+        Object x = this.getObjectUp((String) getStartupParameters().get("objectsName"));
+        if(x instanceof Scrollable) {
+            objectToScroll = (DefaultGuiObject) x;
+        } else {
+            System.out.println("[DEBUG] Caught an exception in " + this.getName() + ". Object is not scrollable.");
+            System.out.println("JSONObject values: ");
+            System.out.println(getStartupParameters().toJSONString());
+        }
+    }
+
+    @Override
+    public JSONObject generateJSONObject() {
+        return super.generateDefaultJSONObject();
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
