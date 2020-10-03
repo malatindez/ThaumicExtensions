f5b891fa53e973230408cbe4b60ec1c3fc52a04a
malatindez <axenance@gmail.com>
2020-10-03 15:41:46 +0300

    Update Button.java

--- src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/Button.java
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/Button.java
@@ -4,30 +4,15 @@ package com.malatindez.thaumicextensions.client.render.misc.gui;
 import org.json.simple.JSONObject;
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector4f;
+import scala.util.parsing.json.JSON;
 
 public class Button extends DefaultGuiObject implements EnhancedGuiScreen.Clickable, EnhancedGuiScreen.Updatable {
     protected DefaultGuiObject icon;
     protected DefaultGuiObject hovered_icon;
-    protected final MethodObjectPair clicked;
-    protected final MethodObjectPair hovered;
+    protected MethodObjectPair clicked;
+    protected MethodObjectPair hovered;
     protected int id;
 
-    @Override
-    public void preInit(String name, Object parent, JSONObject parameters) {
-        if(parameters.containsKey("id")) {
-            id = (int)JsonToFloat(parameters.get("id"));
-        } else {
-            id = 0;
-        }
-        JSONObject icon = (JSONObject) parameters.get(parameters.get("icon"));
-        this.icon = EnhancedGuiScreen.createObject((String) parameters.get("icon"), this, icon);
-        try {
-            JSONObject hovered_icon = (JSONObject) parameters.get(parameters.get("hovered_icon"));
-            this.hovered_icon = EnhancedGuiScreen.createObject((String) parameters.get("hovered_icon"), this, hovered_icon);
-        } catch (Exception ignored) {
-
-        }
-    }
 
     @Override
     public void postInit() {
@@ -37,7 +22,7 @@ public class Button extends DefaultGuiObject implements EnhancedGuiScreen.Clicka
     @SuppressWarnings("unchecked")
     @Override
     public JSONObject generateJSONObject() {
-        JSONObject returnValue = super.generateDefaultJSONObject();
+        JSONObject returnValue = super.generateJSONObject();
         JSONObject a = (JSONObject) returnValue.get(getName());
         a.put("id", (long)id);
         a.put("icon", this.icon.getName());
@@ -50,12 +35,31 @@ public class Button extends DefaultGuiObject implements EnhancedGuiScreen.Clicka
         }
         return returnValue;
     }
+    @Override
+    public void loadFromJSONObject(JSONObject parameters) {
+        super.loadFromJSONObject(parameters);
+        clicked = getMethod(parameters, "clicked", new Class[] {Object.class, int.class});
+        hovered = getMethod(parameters, "hovered", new Class[] {Object.class, int.class});
+
+        if(parameters.containsKey("id")) {
+            id = (int)JsonToFloat(parameters.get("id"));
+        } else {
+            id = 0;
+        }
+
+        JSONObject icon = (JSONObject) parameters.get(parameters.get("icon"));
+        this.icon = EnhancedGuiScreen.createObject((String) parameters.get("icon"), this, icon);
+
+        try {
+            JSONObject hovered_icon = (JSONObject) parameters.get(parameters.get("hovered_icon"));
+            this.hovered_icon = EnhancedGuiScreen.createObject((String) parameters.get("hovered_icon"), this, hovered_icon);
+        } catch (Exception ignored) { }
+
+        this.setSize(this.icon.getSize());
+    }
 
     public Button(String name, Object parent, JSONObject parameters) {
         super(name, parent, parameters);
-        this.setSize(icon.getSize());
-        clicked = getMethod(parameters, "clicked", new Class[] {Object.class, int.class});
-        hovered = getMethod(parameters, "hovered", new Class[] {Object.class, int.class});
     }
 
     @Override
@@ -96,7 +100,9 @@ public class Button extends DefaultGuiObject implements EnhancedGuiScreen.Clicka
 
     @Override
     protected void VectorsWereUpdated() {
-        icon.updateParentBorders(getBorders());
+        if(icon != null) {
+            icon.updateParentBorders(getBorders());
+        }
         if (hovered_icon != null) {
             hovered_icon.updateParentBorders(getBorders());
         }
@@ -137,10 +143,8 @@ public class Button extends DefaultGuiObject implements EnhancedGuiScreen.Clicka
            temp.w > currentMousePosition.y) {
             try {
                 clicked.method.invoke(clicked.object,this, id);
-            } catch (Exception ignored) {
-                return false;
-            }
-            return true;
+                return true;
+            } catch (Exception ignored) { }
         }
         return false;
     }
