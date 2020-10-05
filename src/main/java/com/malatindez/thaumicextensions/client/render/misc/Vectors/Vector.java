dfd0173f0e95119bdbddf81f1cbdd963e0d8c3e5
malatindez <axenance@gmail.com>
2020-10-05 18:27:11 +0300

    Create Vector.java

---
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/Vectors/Vector.java
@@ -0,0 +1,56 @@
+package com.malatindez.thaumicextensions.client.render.misc.Vectors;
+
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.util.vector.Vector4f;
+
+public final class Vector {
+    public static Vector3f mul(Vector3f left, Vector3f right, Vector3f dest) {
+        if(dest == null) {
+            return new Vector3f(left.x * right.x, left.y * right.y, left.z * right.z);
+        }
+        dest.set(left.x * right.x, left.y * right.y, left.z * right.z);
+        return dest;
+    }
+
+    public static Vector3f div(Vector3f left, Vector3f right, Vector3f dest) {
+        if(dest == null) {
+            return new Vector3f(left.x / right.x, left.y / right.y, left.z / right.z);
+        }
+        dest.set(left.x / right.x, left.y / right.y, left.z / right.z);
+        return dest;
+    }
+
+    public static Vector2f mul(Vector2f left, Vector2f right, Vector2f dest) {
+        if(dest == null) {
+            return new Vector2f(left.x * right.x, left.y * right.y);
+        }
+        dest.set(left.x * right.x, left.y * right.y);
+        return dest;
+    }
+
+
+    public static Vector2f div(Vector2f left, Vector2f right, Vector2f dest) {
+        if(dest == null) {
+            return new Vector2f(left.x / right.x, left.y / right.y);
+        }
+        dest.set(left.x / right.x, left.y / right.y);
+        return dest;
+    }
+
+    public static Vector4f mul(Vector4f left, Vector4f right, Vector4f dest) {
+        if(dest == null) {
+            return new Vector4f(left.x * right.x, left.y * right.y, left.z * right.z, left.w * right.w);
+        }
+        dest.set(left.x * right.x, left.y * right.y, left.z * right.z, left.w * right.w);
+        return dest;
+    }
+
+    public static Vector4f div(Vector4f left, Vector4f right, Vector4f dest) {
+        if(dest == null) {
+            return new Vector4f(left.x / right.x, left.y / right.y, left.z / right.z, left.w / right.w);
+        }
+        dest.set(left.x / right.x, left.y / right.y, left.z / right.z, left.w / right.w);
+        return dest;
+    }
+}
