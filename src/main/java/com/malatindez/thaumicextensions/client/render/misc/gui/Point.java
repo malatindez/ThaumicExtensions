2f02c8a3607a87838dab6bc3baec87d2bc4660b8
malatindez <axenance@gmail.com>
2020-10-12 13:42:39 +0300

    create Point.java

---
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/Point.java
@@ -0,0 +1,11 @@
+package com.malatindez.thaumicextensions.client.render.misc.gui;
+
+import org.json.simple.JSONObject;
+
+// This class is being used to automatically scale other elements
+public class Point extends DefaultGuiObject {
+    public Point(String name, Object parent, JSONObject parameters) {
+        super(name, parent, parameters);
+        setSize(0,0 );
+    }
+}
