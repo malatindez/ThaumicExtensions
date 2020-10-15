0d69d8015e084989028ed6b2fd42610ed8354d9e
malatindez <axenance@gmail.com>
2020-10-15 22:03:00 +0300

    Create Selector.java

---
+++ src/main/java/com/malatindez/thaumicextensions/client/render/misc/gui/Selector.java
@@ -0,0 +1,13 @@
+package com.malatindez.thaumicextensions.client.render.misc.gui;
+
+import org.json.simple.JSONObject;
+
+public class Selector extends Collection {
+    public Selector(String name, Object parent, JSONObject parameters) {
+        super(name, parent, parameters);
+    }
+    @Override
+    public void loadFromJSONObject(JSONObject parameters) {
+        super.loadFromJSONObject(parameters);
+    }
+}
