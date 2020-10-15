d31bd784ba89d8ed42d92b4cab0ba09323c316ed
malatindez <axenance@gmail.com>
2020-10-15 22:03:06 +0300

    Create GuiEditorContextMenu.java

---
+++ src/main/java/com/malatindez/thaumicextensions/client/render/gui/GuiEditorMisc/GuiEditorContextMenu.java
@@ -0,0 +1,36 @@
+package com.malatindez.thaumicextensions.client.render.gui.GuiEditorMisc;
+
+import com.malatindez.thaumicextensions.client.render.misc.gui.Collection;
+import com.malatindez.thaumicextensions.client.render.misc.gui.DefaultGuiObject;
+import com.malatindez.thaumicextensions.client.render.misc.gui.EnhancedGuiScreen;
+import com.malatindez.thaumicextensions.client.render.misc.gui.ScrollBar;
+import org.json.simple.JSONObject;
+
+public class GuiEditorContextMenu extends Collection implements ScrollBar.Scrollable {
+    public GuiEditorContextMenu(String name, Object parent, JSONObject parameters) {
+        super(name, parent, parameters);
+    }
+
+    @Override
+    public void setOffsetX(float offset) {
+       // offsetX = offset;
+      //  updateHideStates();
+    }
+
+    @Override
+    public void setOffsetY(float offset) {
+        //offsetY = offset;
+       // updateHideStates();
+    }
+
+    @Override
+    public float getOffsetX() {
+        return 0;
+    }
+
+    @Override
+    public float getOffsetY() {
+        return 0;
+    }
+
+}
